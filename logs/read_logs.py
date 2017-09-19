d585b7f9c833f8b4165148d7e0f923712c379b66
Juan E Rolon <rolon.math@gmail.com>
2017-09-19 11:55:15 -0500

    added maxq action symbol

--- logs/read_logs.py
+++ logs/read_logs.py
@@ -53,12 +53,15 @@ ax = plt.axes()
 plt.xlim(-1.2,1.2)
 plt.ylim(-0.8,1.8)
 
-circle = plt.Circle((-0.8, 1.5), radius=0.15, fc='g')
+#green traffic light
+circle = plt.Circle((-0.8, 1.5), radius=0.15, fc='lime')
 plt.gca().add_patch(circle)
 
+#red traffic light
 circle2 = plt.Circle((-0.45, 1.5), radius=0.15, fc='r')
 plt.gca().add_patch(circle2)
 
+#intersection point
 intersect = plt.Circle((0.05, 0.55), radius=0.05, fc='k', fill=False,linestyle='dashed')
 plt.gca().add_patch(intersect)
 
@@ -99,9 +102,8 @@ ax.arrow(xi, yi, dx,dy, head_width=0.04, head_length=0.05, fc='g', ec='g')
 plt.text(0.45,0.6,'Oncoming right',fontsize='9')
 
 
-
 #waypoint foward
-if False:
+if True:
     xi = 0.05
     yi = -0.5
     dx = 0.0
@@ -111,18 +113,44 @@ if False:
 
 
 #waypoint right
-xi = 0.05
-yi = -0.5
-dx = 0.0
-dy = 0.15
-ax.arrow(xi, yi, dx,dy, head_width=0.04, head_length=0.05, fc='k', ec='k',linestyle='solid',linewidth=3.5)
-xi2 = 0.05
-yi2 = -0.3
-dx2 = 0.1
-dy2 = 0.0
-ax.arrow(xi2, yi2, dx2,dy2, head_width=0.04, head_length=0.05, fc='k', ec='k',linestyle='solid',linewidth=3.5)
+if False:
+    # ---- base
+    xi = 0.05
+    yi = -0.5
+    dx = 0.0
+    dy = 0.15
+    ax.arrow(xi, yi, dx,dy, head_width=0.04, head_length=0.05, fc='k', ec='k',linestyle='solid',linewidth=3.5)
+    # ---- tip
+    xi2 = 0.05
+    yi2 = -0.3
+    dx2 = 0.1
+    dy2 = 0.0
+    ax.arrow(xi2, yi2, dx2,dy2, head_width=0.04, head_length=0.05, fc='k', ec='k',linestyle='solid',linewidth=3.5)
+    plt.text(-0.1,-0.6,'Waypoint',fontsize='9',fontweight='bold')
 
-plt.text(-0.1,-0.6,'Waypoint',fontsize='9',fontweight='bold')
+#waypoint left
+if False:
+    # ---- base
+    xi = 0.05
+    yi = -0.5
+    dx = 0.0
+    dy = 0.15
+    ax.arrow(xi, yi, dx,dy, head_width=0.04, head_length=0.05, fc='k', ec='k',linestyle='solid',linewidth=3.5)
+    # ---- tip
+    xi2 = 0.05
+    yi2 = -0.3
+    dx2 = -0.1
+    dy2 = 0.0
+    ax.arrow(xi2, yi2, dx2,dy2, head_width=0.04, head_length=0.05, fc='k', ec='k',linestyle='solid',linewidth=3.5)
+    plt.text(-0.1,-0.6,'Waypoint',fontsize='9',fontweight='bold')
+
+# maxQ action forward
+xi = 0.7
+yi = 1.35
+dx = 0.0
+dy = 0.3
+ax.arrow(xi, yi, dx, dy, head_width=0.04, head_length=0.05, fc='darkgreen', ec='darkgreen', linestyle='solid', linewidth=3.5)
+plt.text(0.5, 1.25, 'MaxQ action', fontsize='9', fontweight='bold', color='darkgreen')
 
 #ax.axis('off')
 plt.show()
